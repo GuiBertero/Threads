403774036e52ecb77c9fb8a54856db26e513a155
GuiBertero <guibcoliveira@gmail.com>
2026-04-24 16:40:01 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+class Program
+{
+    static string[] setores = {
+        "Emergencia", "UTI", "Enfermaria", "Radiologia",
+        "Laboratorio", "Farmacia", "CentroCirurgico", "Recepcao",
+        "Cardiologia", "Neurologia", "Pediatria", "Oncologia",
+        "Ortopedia", "Dermatologia", "Psiquiatria", "Nutricao",
+        "Fisioterapia", "Administracao", "TI", "Seguranca"
+    };
+
+    static void Main()
+    {
+        string caminho = "hospital_sem_lock.txt";
+
+        if (File.Exists(caminho))
+            File.Delete(caminho);
+
+        Thread[] threads = new Thread[20];
+
+        for (int i = 0; i < 20; i++)
+        {
+            int id = i;
+
+            threads[i] = new Thread(() =>
+            {
+                Random rnd = new Random();
+
+                for (int j = 0; j < 200; j++)
+                {
+                    string log = $"{DateTime.Now:HH:mm:ss.fff} | {setores[id]} | Paciente {rnd.Next(1000, 9999)} | Evento {j}";
+
+                    // ❌ SEM CONTROLE
+                    File.AppendAllText(caminho, log + "\n");
+
+                    Console.WriteLine(log);
+
+                    Thread.Sleep(rnd.Next(5, 20));
+                }
+            });
+
+            threads[i].Start();
+        }
+
+        foreach (var t in threads)
+            t.Join();
+
+        Console.WriteLine("\nFinalizado SEM controle.");
+    }
+}
---
+++ threadsComControle.cs
@@ -0,0 +1,52 @@
+using System;
+using System.IO;
+using System.Threading;
+
+class Program
+{
+    static object locker = new object();
+
+    static string[] setores = {
+        "Emergencia", "UTI", "Enfermaria", "Radiologia", "CentroCirurgico", "Cardiologia", "Neurologia", "Pediatria", "Ortopedia", "Dermatologia", "Psiquiatria"
+    };
+
+    static void Main()
+    {
+        string caminho = "hospital_com_lock.txt";
+
+        if (File.Exists(caminho))
+            File.Delete(caminho);
+
+        Thread[] threads = new Thread[setores.Length];
+
+        for (int i = 0; i < setores.Length; i++)
+        {
+            int id = i;
+
+            threads[i] = new Thread(() =>
+            {
+                Random rnd = new Random();
+
+                for (int j = 0; j < 200; j++)
+                {
+                    string log = $"{DateTime.Now:HH:mm:ss.fff} | {setores[id]} | Paciente {rnd.Next(1000, 9999)} | Evento {j}";
+
+                    lock (locker)
+                    {
+                        File.AppendAllText(caminho, log + "\n");
+                        Console.WriteLine(log);
+                    }
+
+                    Thread.Sleep(rnd.Next(5, 20));
+                }
+            });
+
+            threads[i].Start();
+        }
+
+        foreach (var t in threads)
+            t.Join();
+
+        Console.WriteLine("\nFinalizado COM controle.");
+    }
+}	
